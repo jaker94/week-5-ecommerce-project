e36217d91d70da01bf1a88d81c8ca7f0970e2212
Jacob <jakeregister94@gmail.com>
2023-02-23 20:07:10 -0700

    1:08 removed div causing issues, working on /books JS filter function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import Discounted from './components/Discounted';
+import Explore from './components/Explore';
 import Featured from './components/Featured';
+import Footer from './components/Footer';
 import Highlights from './components/Highlights';
 import Landing from './components/Landing';
 import Nav from './components/Nav'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Books from './pages/Books';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-   <Nav />
-   <Landing />
-   <Highlights />
-   <Featured />
-   <Discounted />
+    <Nav />
+    <Route path="/" exact component={Home}/>
+    <Route path="/books" component={Books} />
+   <Footer />
     </div>
+    </Router>
   );
 }
 
