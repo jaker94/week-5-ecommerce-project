5f0682698bd98b4ddf8c2f2c14678877a052c789
Jacob <jakeregister94@gmail.com>
2023-02-24 09:56:10 -0700

    1:22 fixed react-router-dom 5.3.0 and react18 compatibility issue by reinstalling react-router-dom to 5.3.3, completed all books page with working filter function, created bookinfo.jsx for individual books but it is in complete

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import {
   faTags,
   faStar,
   faStarHalfAlt,
+  faArrowLeft
 } from "@fortawesome/free-solid-svg-icons";
 
 library.add(
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,8 +1,40 @@
 import React from "react";
 import { books } from "../data";
 import Book from "../components/ui/Book";
+import { useState } from "react";
 
-const Books = () => {
+const Books = ({ books: initialBooks }) => {
+  const [books, setBooks] = useState(initialBooks);
+
+  function filterBooks(filter) {
+    console.log(filter);
+    if (filter === "LOW_TO_HIGH") {
+      setBooks(
+        books
+          .slice()
+          .sort(
+            (a, b) =>
+              (a.salePrice || a.originalPrice) -
+              (b.salePrice || b.originalPrice)
+          )
+      );
+    }
+    if (filter === "HIGH_TO_LOW") {
+      setBooks(
+        books
+          .slice()
+          .sort(
+            (a, b) =>
+              (b.salePrice || b.originalPrice) -
+              (a.salePrice || a.originalPrice)
+          )
+      );
+    }
+    if (filter === "RATING") {
+      setBooks(books.slice().sort((a, b) => b.rating - a.rating));
+    }
+  }
+
   return (
     <div className="books__body">
       <main id="books__main">
@@ -13,16 +45,22 @@ const Books = () => {
                 <h2 className="section__title books__header--title">
                   All Books
                 </h2>
-                <select id="filter" defaultValue="DEFAULT">
-                    <option value="DEFAULT" disabled>Sort</option>
-                    <option value="LOW_TO_HIGH">Price, Low to High</option>
-                    <option value="HIGH_TO_LOW">Price, High to Low</option>
-                    <option value="RATING">Rating</option>
+                <select
+                  id="filter"
+                  defaultValue="DEFAULT"
+                  onChange={(event) => filterBooks(event.target.value)}
+                >
+                  <option value="DEFAULT" disabled>
+                    Sort
+                  </option>
+                  <option value="LOW_TO_HIGH">Price, Low to High</option>
+                  <option value="HIGH_TO_LOW">Price, High to Low</option>
+                  <option value="RATING">Rating</option>
                 </select>
               </div>
               <div className="books">
                 {books.map((book) => (
-                    <Book book={book} key={book.id} />
+                  <Book book={book} key={book.id} />
                 ))}
               </div>
             </div>
